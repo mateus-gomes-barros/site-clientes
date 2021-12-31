1448b98aa29f2246246163dab558cc50e443e106
Mateus Gomes barros <90994257+mateus-gomes-barros@users.noreply.github.com>
2021-12-31 17:34:18 -0300

    Add files via upload

---
+++ chuva.inc/js/posts.js
@@ -0,0 +1,51 @@
+var posts = [
+  
+  {
+    "imagem": "https://picsum.photos/id/0/5616/3744",
+    "titulo": "Hackathon",
+    "autor": "Mateus",
+    "data": "31/12/2021",
+    "imagem_post": "https://picsum.photos/id/1/5616/3744",
+    "texto": "1º EVENTO: O Hackathon é uma espécie de maratona de criatividade, realizado em um curto período de tempo, onde equipes partem em busca de soluções inovadoras para algum problema específico. Além de palestras dos mais diversos profissionais, como professores, engenheiros, gamers... Esse tipo de evento está atraindo cada vez mais público que deseja entrar para o mundo da tecnologia/informática."
+  },
+  {
+    "imagem": "https://picsum.photos/id/1029/4887/2759",
+    "titulo": "Inova Day",
+    "autor": "Mateus",
+    "data": "31/12/2021",
+    "imagem_post": "https://picsum.photos/id/1031/5446/3063",
+    "texto": "2º EVENTO: O Inova Day é um festival de palestras, exposições e oficinas que acontecem de forma simultânea, em 3 diferentes espaços, durante um dia inteiro. Essas atividades tem como objetivo compartilhar com a comunidade local um pouco do conteúdo que o Inova Maranhão, tanto disseminam através dos programas: a Ciência, Tecnologia e Inovação. Contando com exposição de trabalhos, projetos e ideias onde qualquer um dos participantes podem apresentar no evento.",
+    
+  },
+  {
+    "imagem": "https://picsum.photos/id/1077/3000/1995",
+    "titulo": "Programa de Startups.",
+    "autor": "Mateus",
+    "data": "31/12/2021",
+    "imagem_post": "https://picsum.photos/id/1079/4496/3000",
+    "texto": "3º EVENTO: oportunidade de fazer conexões com as empresas que acabaram de receber recursos financeiros para desenvolverem suas ideias de negócios, e certamente essas startups precisarão de pessoas para auxiliar no desenvolvimento de produtos tecnológicos. Resumindo, mais público e mais necessidade de se organizar eventos grandes sem problemas, atraindo mais clientes no ramo tecnológico mais jovens.",
+    
+  },
+  
+  {
+    "imagem": "https://picsum.photos/id/137/4752/3168",
+    "titulo": "Critérios e motivos. Recomendação: Ouvir o lo-fi no final da pagina",
+    "autor": "Mateus",
+    "data": "31/12/2021",
+    "imagem_post": "https://picsum.photos/id/146/5184/3456",
+    "texto": "Onde e como foi feita a busca?<br>Busquei referenciais do meu meio social. Fui bolsista no programa Inova Maranhão e posso afirmar que o mercado de programadores, designers e cientistas de dados está cada vez mais encorpado e precisando de cada vez mais pessoas. Ví nesses eventos uma ótima opção, pois em cada um desses eventos que citei nos posts anteriores, todos eles eu tive que baixar um aplicativo de compra de ingressos para garantir meu lugar. E cada vez mais as escolas estão criandos eventos como esses, professores iniciando eventos para ensinar programação, eventos para exposição de protótipos.<br><br>Um dos critérios usados por mim para selecionar esses eventos é a frequencia que esses eventos ocorrem, chegando a ser 1 vez por semana. Caso consigamos nos destacar nesse meio de organização de eventos  e automatização de processos, seriamos referencia para que outras pessoas interessadas em iniciar algum evento próprio acadêmico ou científico com nossa plataforma. E como o networking se expandindo, seria questão de tempo para mais trafégo na plataforma Galoá.<br><br> Mas por qual motivo esses eventos?<br><br>Simples! Um deles é o fato de haver dezenas, senão, centenas, de estudantes em um só eventos. Ou seja, se um desses eventos faz uso de certificados para quem participou, pagamentos pelo próprio app...Será notada a praticidade como todos os processos são resolvidos, sem necessidade de burocracia ou perda de tempo com documentação. Nosso sistema será mais utilizado por esses antes estudantes, os quais farão seus eventos ou minicursos. A médio prazo essas opções se tornam cada vez mais promissoras. Os hackathons devem ter um alto planejamento e preparo, pois quando estas etapas bem cumpridas o evento se faz um sucesso. Por conta da necessidade deste alto planejamento, do interesse de levar o conhecimento da academia para o mercado e da disposição do mercado para realizar eventos assim.<br><br> O diferencial para trazer mais 10 eventos seria nada mais que aumentar o networking entre iniciativas públicas e privadas (relaxa, não é uma proposta de intervenção para o enem) Mas o que eu quis dizer? Me colocando como exemplo, todos os dias entre setembro e novembro desse anos de 2021, aconteceram centenas de eventos cientificos, exposições, minicuros, palestras...E todos eles usavam apenas um app,para organizar somente os ingressos. Alguns dos minicuros emitiam certificados próprios, mas nada muito atrante o suficente para trazer credibilidade do evento, manchando a reputação dos organizadores. Então, o pontapé inicial partiria de nós mesmos para apresentar nossa plataforma e os beneficios que o uso definitivo dela para todos esses eventos traria para esse novo público que está surgindo",
+    "video": "https://www.youtube.com/embed/AzV77KFsLn4"
+  },
+  
+  
+  {
+    "imagem": "imagens/mateus-1.png",
+    "titulo": "Sobre Mim:)",
+    "autor": "Mateus",
+    "data": "24/03/1999",
+    "imagem_post": "imagens/mateus-2.png",
+    "texto": "Agora um vídeo sobre mim e minha trajetória acadêmica (além de gatilhos)",
+    "video": "https://www.youtube.com/embed/bPzDlnDriKU"
+  },
+ 
+];
